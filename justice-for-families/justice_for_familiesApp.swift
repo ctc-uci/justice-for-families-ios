843efa6c41f3ac647911e5afdb9ff417c62b485c
nguyenethan01 <nguyenethan01@gmail.com>
2020-10-24 10:34:21 -0700

    Initial Commit

---
+++ justice-for-families/justice_for_familiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  justice_for_familiesApp.swift
+//  justice-for-families
+//
+//  Created by Ethan  Nguyen on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct justice_for_familiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
