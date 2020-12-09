b5b1e005da74bed950d504290b607591d3e552e7
Jules Labador <juleslabador@gmail.com>
2020-12-09 14:34:12 -0800

    Built out the popup view to add a post. "Choose a community", "Title", and "What's on your mind" fields have been created. Couldn't get the navigation bar buttons to show up though, gonna need more time to look at that.

--- justice-for-families/Post.swift
+++ justice-for-families/Post.swift
@@ -14,55 +14,65 @@ struct Post: View {
     @State private var showModal = false
     
     var body: some View {
+//        PopUp()
         Button(action:{
             self.showModal.toggle()
         }){
             Text("Show modal")
-            
+
         }.sheet(isPresented: $showModal){
             PopUp()
         }
-
-
     }
 }
 
 struct PopUp: View{
     
+    @State var title: String = ""
+    @State var postBody: String = "What's on your mind?"
     @Environment(\.presentationMode) private var presentationMode
     
-    @State var title: String = ""
-    @State private var message = ""
-    @State private var textStyle = UIFont.TextStyle.body
-    
-    var body: some View{
+    var body: some View {
         NavigationView {
-            Form{
-                Section{
-                    Text("hello world popup")
-                    VStack(alignment: .leading) {
-                        TextField("Title", text: $title)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                        TextView(text: $message, textStyle: $textStyle)
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("replace this later")
+                        .frame(width: 60, height: 60)
+                        .background(Color.gray)
+                        .cornerRadius(30)
+                        .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 0))
+                    Text("Choose a community")
+                        .frame(width: .infinity, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.secondary)
+                        .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 24))
                 }
+                Divider()
+                TextField("Title (required)", text: $title)
+                    .frame(width: .infinity, height: 82, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black)
+                    .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+                Divider()
+                TextEditor(text: $postBody)
+                    .font(.custom("Robot-Regular", size: 14))
+                    .foregroundColor(.secondary)
+                    .frame(width: .infinity, height: 160, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+                Spacer()
             }
-            
-            .navigationBarItems(leading:          Button(action: {
-                self.presentationMode.wrappedValue.dismiss()
-            }){
-                    Image(systemName: "xmark").font(.system(size: 16, weight: .regular))
-            }, trailing:
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }){
-                        Text("POST")
-                    
-                }
-            )
-            
-            
+            .navigationBarTitle("Text Post")
+            .navigationBarTitleDisplayMode(.inline)
+            .font(.custom("Roboto-Bold", size: 16))
         }
+        .navigationBarItems(leading: Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.system(size: 16, weight: .regular))
+        }), trailing: Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Text("POST")
+        }))
     }
 }
 
