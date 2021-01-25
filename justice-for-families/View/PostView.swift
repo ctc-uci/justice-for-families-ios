d3eafdef623f8689f9499baaaa071235e2d25d30
Joshua Chan <joshualchan@gmail.com>
2021-01-25 12:42:02 -0800

    added navigation bar items in postview

--- justice-for-families/View/PostView.swift
+++ justice-for-families/View/PostView.swift
@@ -1,55 +1,62 @@
 //
-//  PostScreenView.swift
-//  justice-for-families
+// PostScreenView.swift
+// justice-for-families
 //
-//  Created by Joshua Chan on 12/4/20.
+// Created by Joshua Chan on 12/4/20.
 //
-
 //https://www.youtube.com/watch?v=yTWPsC6DJFo
 //Post ID should get passed to this screen
-
 import SwiftUI
-
 struct PostView: View {
-    
-    @State var txt = ""
-    @State var height : CGFloat = 20
-    
-    var edges = UIApplication.shared.windows.first?.safeAreaInsets
-    //@StateObject var postData = PostViewModel()
-    var body: some View {
-        ScrollView{
-            VStack{
-                PostRow()
-                CommentRow()
-            }
-            .padding()
-            .background(Constants.backgroundColor)
+  @State var txt = ""
+  @State var height : CGFloat = 20
+  var edges = UIApplication.shared.windows.first?.safeAreaInsets
+  //@StateObject var postData = PostViewModel()
+  var body: some View {
+      ScrollView{
+        VStack{
+          PostRow()
+          CommentRow()
         }
-        
-        HStack(spacing:8){
-            ResizeableTF(txt: self.$txt, height: self.$height)
-                .frame(height:self.height < 150 ? self.height : 150)
-                .padding(.horizontal)
-                .background(Constants.backgroundColor)
-                .cornerRadius(15)
-            Button(action: {}){
-                Text("Send")
-                .padding(.top, 6)
-                .padding(.bottom, 6)
-                .padding(.horizontal)
-                .foregroundColor(.white)
-                .background(Constants.backgroundColor)
-            }
-            .cornerRadius(15)
-            .padding(.vertical,4)
+        .padding()
+        .background(Constants.backgroundColor)
+      }
+      .navigationBarItems(trailing:
+                  HStack(spacing: 120) {
+                    Button(action: {
+                      print("Menu Button Pressed...")
+                    }) {
+                      Text("...")
+                    }
+                    Button(action: {
+                      print("Save button Pressed...")
+                    }) {
+                      Image(systemName: "bookmark")
+                    }
+                  }
+                )
+      HStack(spacing:8){
+        ResizeableTF(txt: self.$txt, height: self.$height)
+          .frame(height:self.height < 150 ? self.height : 150)
+          .padding(.horizontal)
+          .background(Constants.backgroundColor)
+          .cornerRadius(15)
+        Button(action: {}){
+          Text("Send")
+          .padding(.top, 6)
+          .padding(.bottom, 6)
+          .padding(.horizontal)
+          .foregroundColor(.white)
+          .background(Constants.backgroundColor)
         }
-        .padding(.horizontal)
-    }
+        .cornerRadius(15)
+        .padding(.vertical,4)
+      }
+      .padding(.horizontal)
+  }
 }
-
 struct PostView_Previews: PreviewProvider {
-    static var previews: some View {
-        PostView()
-    }
+  static var previews: some View {
+    PostView()
+  }
 }
