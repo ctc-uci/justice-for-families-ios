3603ae33464da54b3f94a9fcd454cbfd50663eb4
Cyre Jorin To <cyrejorin@gmail.com>
2021-02-28 21:29:50 +0000

    updated highres for signup/login

--- justice-for-families/View/LoginView.swift
+++ justice-for-families/View/LoginView.swift
@@ -18,90 +18,99 @@ struct LoginView: View {
     private var fieldWidth: CGFloat? = 350
     private var fieldHeight: CGFloat? = 60
     var body: some View {
-        
         NavigationView {
-           
+            ScrollView{
             VStack (alignment: .center, spacing: 20) {
-                Image("testLogo")
+                Spacer()
+                Spacer()
+                Image("J4F-Logo")
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
-             
-         
+                    .scaledToFill() // <=== Saves aspect ratio
+                    .frame(width: 270.0, height:62)
+                    .padding(.top,35)
+                
+
                 VStack(alignment: .leading, spacing: -30) {
                     Text("Welcome,")
                         .padding()
-                        .font(.system(size: 20, weight: .heavy))
+                        .font(.custom("Poppins-Medium", size: 24))
+                        .foregroundColor(Constants.primaryFontColor)
                     Text("Sign in to continue!")
                         .padding()
-                }.frame(width: fieldWidth, height: fieldHeight, alignment: .leading)
+                        .font(.custom("Poppins-Regular", size: 18))
+                        .foregroundColor(Constants.secondaryFontColor)
+                }   .frame(width: fieldWidth, height: fieldHeight, alignment: .leading)
+                    .padding(.bottom,25)
 
                 
-                VStack(alignment: .leading, spacing: 25){
-                    TextField("Username",text: self.$email)
-                        .padding()
-                        .background(Capsule().fill(Color.white))
-                        .frame(width: fieldWidth, height: fieldHeight, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        
+                VStack(alignment: .center){
+                    HStack{
+                        TextField("email",text: self.$email)
+                            .foregroundColor(Constants.primaryFontColor)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(20)
+                            .padding(.bottom,26)
+                            .frame(width:345,height:53)
+                            .font(.custom("Poppins-Regular", size: 16))
+                    }
+                    
                     HStack {
                         if self.hiddenPass {
-                            SecureField("Password", text: self.$password)
+                            SecureField("password", text: self.$password).background(Color.white).frame(width:278).foregroundColor(Constants.primaryFontColor).font(.custom("Poppins-Regular", size: 16))
                         } else {
-                            TextField("Password", text: self.$password)
+                            TextField("password", text: self.$password).background(Color.white).frame(width:278).foregroundColor(Constants.primaryFontColor).font(.custom("Poppins-Regular", size: 16))
                         }
                         
                         Button(action: {self.hiddenPass.toggle()}) {
                             Image(systemName: !self.hiddenPass ? "eye.fill": "eye.slash.fill")
                                 .foregroundColor((!self.hiddenPass) ? Color.green : Color.secondary)
-                        } .padding([.leading], 0)
+                        }
                         
                     }   .padding()
-                        .frame(width: fieldWidth, height: fieldHeight, alignment: .center)
-                        .background(Capsule().fill(Color.white))
+                        .overlay(RoundedRectangle(cornerRadius:20).stroke(Color.white,lineWidth:1)).background(RoundedRectangle(cornerRadius: 20).fill(Color.white))
                     
-                }.padding([.leading, .trailing], 27.5)
+                    
+                    
+                    Button(action: {}, label: {
+                        Text("Forgot password?")
+                            .font(.custom("Poppins-Regular", size: 13))
+                            .foregroundColor(Constants.primaryFontColor)
+                    })
+                    .frame(width: fieldWidth, height: CGFloat(fieldHeight!/2), alignment: .trailing)
+                }
+                .padding(.bottom,30)
                 
-                Button(action: {
-                    Network.createNewPost(parameters: [
-                        "text": "Text",
-                        "username": "JulesLabador",
-                        "tags": [
-                            "tag1",
-                            "tag2"
-                        ],
-                        "numComments": 5,
-                        "title": "New Post",
-                        "anonymous": false,
-                        "numLikes": 5
-                    ])
-                }){
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Login")
-                        .foregroundColor(.white)
+                        .font(.custom("Poppins-Regular", size: 18))
+                        .foregroundColor(Color.white)
+                        .fontWeight(.heavy)
                         .padding()
                         .frame(width: fieldWidth, height: fieldHeight, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .background(Capsule().fill(Color.gray))
-                        .cornerRadius(15.0)
-                }
-                Button(action: {}, label: {
-                    Text("Forgot password?")
-                        .fontWeight(.medium)
-                }).foregroundColor(.black)
-                .frame(width: fieldWidth, height: CGFloat(fieldHeight!/2), alignment: .trailing)
-                Spacer()
+                        .background(Capsule().fill(Constants.grey))
+                        
+                }.padding([.leading, .trailing],30)
+                
+                
                 HStack(alignment: .center, spacing: 0){
                     Text("I'm a new user. ")
-                        .font(.system(size: 15))
+                        .font(.custom("Poppins-Regular", size: 16))
+                        .foregroundColor(Constants.primaryFontColor)
                     NavigationLink(destination: SignUpView()){
-                        Text("Join Now!")
-                            .foregroundColor(.black)
-                            .font(.system(size: 15, weight: .heavy))
+                        Text("Sign Up")
+                            .foregroundColor(Constants.tertiaryFontColor)
+                            .font(.custom("Poppins-Medium", size: 16))
                     }
                 }
                 Spacer()
-            }.background(Constants.backgroundColor)
-            .navigationBarHidden(true)
+            }
+            
+        }.navigationBarHidden(true)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Constants.loginBackground).ignoresSafeArea()
         }.navigationBarHidden(true)
     }
-
 }
 
 struct LoginView_Previews: PreviewProvider {
