ae000da2dcd2e9940df3558373329833f3893d22
Jules Labador <jlabador@gmail.com>
2021-04-20 22:11:16 +0000

    Started working on implementing Figma designs for the PostView. Decided to...

--- justice-for-families/Home View/HomeFeed.swift
+++ justice-for-families/Home View/HomeFeed.swift
@@ -34,36 +34,22 @@ class NetworkManager: ObservableObject {
     
     public func fetchPosts() {
         Network.fetchAllPosts { (posts) in
-            DispatchQueue.main.async {
-                self.posts = posts
-            }
+            self.posts = posts
+            posts.forEach({
+                print($0)
+            })
         }
     }
     
-}
-
-struct J4FColors {
     
-    //static let primaryText = Color("primaryText")
-    static let primaryText = Color(.black)
+    
 }
 
-struct J4FFonts {
-    static let sectionTitle = Font.custom("AvenirNext-Medium", size: 15)
-    static let headline = Font.custom("AvenirNext-Medium", size: 15)
-    static let button = Font.custom("AvenirNext-Regular", size: 10)
-    static let username = Font.custom("AvenirNext-Medium", size: 13)
-}
-
-
-
-struct UIFeed: View {
-    @ObservedObject var networkManager: NetworkManager
-    private let posts: [Post] = [
-//        Post(id: UUID(), text: "Test 1"),
-//        Post(id: UUID(), text: "Test 2"),
-//        Post(id: UUID(), text: "Test 3")
-    ]
+struct HomeFeed: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    private let posts: [Post] = []
         
     private let updates: [Update] = [
         .init(numberOfLikes: 1, numberOfComments: 1),
@@ -94,142 +80,61 @@ struct UIFeed: View {
     
     
     var body: some View {
-        List {
-            Section(header: WhatYouMissedSectionHeader()) {
-                ScrollView(.horizontal, showsIndicators: false, content: {
-                    HStack {
-                        ForEach(1..<100) { i in
-                            WhatYouMissedCell()
+        NavigationView {
+            List {
+                
+                Section(header: SectionHeader(title: "Tags you follow")){
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        HStack {
+                            ForEach(1..<100) { i in
+                                TagCell(tag: "Resources")
+                                    .background(J4FColors.background)
+                            }
+                        }.background(J4FColors.background)
+                    })
+                    .listRowInsets(EdgeInsets())
+                    .background(J4FColors.background)
+                }.textCase(.none)
+                
+                Section(header: SectionHeader(title: "What you missed...")){
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        HStack {
+                            ForEach(1..<100) { i in
+                                WhatYouMissedCell()
+                            }
                         }
+                    })
+                    .listRowInsets(EdgeInsets())
+                    .background(J4FColors.background)
+                }.textCase(.none)
+                
+                Section(header: SectionHeader(title: "Feed")) {
+                    ForEach(networkManager.posts) { p in
+                        FeedCell(post: p)
+                            .listRowBackground(J4FColors.background)
                     }
-                })
-            }.textCase(.none)
-            
-            Section(header: FeedSectionHeader()) {
-                ForEach(networkManager.posts) { p in
-                    FeedCell(post: p)
-                }
-            }.textCase(.none)
+                }.textCase(.none)
+                .listStyle(SidebarListStyle())
+            }
+            .navigationTitle("J4F")
         }
   
     }
 }
     
 
-struct WhatYouMissedSectionHeader: View {
+struct SectionHeader: View {
+    
+    let title: String
+    
     var body: some View {
-        Text("What you missed...")
+        Text(title)
+            .padding(EdgeInsets(top: 16, leading: 16, bottom: 8, trailing: 0))
             .textCase(.none)
             .font(J4FFonts.sectionTitle)
-            .foregroundColor(J4FColors.primaryText)
-    }
-}
-
-struct WhatYouMissedCell: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 5, style: .continuous)
-                .fill(Color(.sRGB, red: 103/255.0, green: 154/255.0, blue: 151/255.0, opacity: 0.20))
-            VStack {
-                Text("Update")
-            }
-            .padding(20)
-        }
-        .frame(width: 140, height: 80)
-    }
-    
-}
-
-struct FeedSectionHeader: View {
-    var body: some View {
-        Text("Feed")
-            .textCase(.none)
-            .font(J4FFonts.sectionTitle)
-            .foregroundColor(J4FColors.primaryText)
-    }
-}
-
-struct FeedCellInteractButtons: View {
-    
-    let numLikes: Int
-    let numComments: Int
-    
-    var body: some View {
-        HStack {
-            
-            Button(action: {
-                print("Tapped on the like button!")
-            }) {
-                HStack(alignment: .center) {
-                    Image(systemName: "hand.thumbsup")
-                    Text("\(self.numLikes)")
-                        .font(J4FFonts.button)
-                        .foregroundColor(J4FColors.primaryText)
-                }
-                
-            }
-            Spacer()
-            Button(action: {
-                print("Tapped on the comment button!")
-            }) {
-                HStack(alignment: .center) {
-                    Image(systemName: "bubble.left")
-                    Text("\(self.numComments)")
-                        .font(J4FFonts.button)
-                        .foregroundColor(J4FColors.primaryText)
-                }
-                
-            }
-            Spacer()
-            Button(action: {
-                print("Tapped on the like button!")
-            }) {
-                HStack(alignment: .center) {
-                    Image(systemName: "square.and.arrow.up")
-                    Text("10 shares")
-                        .font(J4FFonts.button)
-                        .foregroundColor(J4FColors.primaryText)
-                }
-                
-            }
-            
-        }
-    }
-}
-
-struct FeedCell: View {
-    
-    let post: Post
-    
-    var body: some View {
-        NavigationLink(destination: PostView(post: post)) {
-            ZStack {
-                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                    .fill(Color.white)
-                VStack(alignment: .leading) {
-                    HStack {
-                        Image(systemName: "person.crop.circle")
-                            .resizable()
-                            .frame(width: 41, height: 41, alignment: .leading)
-                        
-                        Text(post.username)
-                            .font(J4FFonts.username)
-                            .foregroundColor(J4FColors.primaryText)
-                        Spacer()
-                    }
-                    
-                    Spacer(minLength: 16)
-                    Text(post.title)
-                        .font(J4FFonts.headline)
-                        .foregroundColor(J4FColors.primaryText)
-                        .multilineTextAlignment(.leading)
-                    Spacer(minLength: 16)
-                    FeedCellInteractButtons(numLikes: 5, numComments: post.numComments)
-                    
-                }
-                .padding(20)
-            }
-        }
+            .frame(width: UIScreen.main.bounds.width, height: 60, alignment: .leading)
+            .background(J4FColors.background)
+            .foregroundColor(J4FColors.darkBlue)
     }
 }
 
@@ -305,8 +210,7 @@ struct HomeFeedHelper: UIViewRepresentable {
 struct HomeFeed_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            HomeFeed(model: model)
-//            WhatYouMissedSection()
+            HomeFeed()
         }
     }
 }*/
